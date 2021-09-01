68325c974aad3af00054c8ac9d04d013792cf252
wjz <279646436@qq.com>
2021-09-01 16:30:12 +0800

    feat: 添加网络请求

---
+++ src/network/request.js
@@ -0,0 +1,27 @@
+/**
+ * @Dsc: 注释
+ * @Author wjz
+ * @Date 2021/9/1 15:01
+ */
+import axios from 'axios'
+
+export  function request(config) {
+  const  instance = axios.create({
+    baseURL:'http://192.168.12.53:9012/TestWebService.asmx',
+    dataType: 'json',
+    contentType: 'application/json;charset=GB2312',
+    timeout:5000
+  })
+
+  instance.interceptors.request.use(config=>{
+    return config
+  },error => {})
+
+  instance.interceptors.response.use(res=>{
+    return res.data
+  },err=>{
+
+  })
+
+  return instance(config)
+}
