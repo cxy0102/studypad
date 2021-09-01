db1199da5e14a6da1ed85686b307890d924384ce
wjz <279646436@qq.com>
2021-09-01 23:22:32 +0800

    feat: 新增底部菜单栏

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,66 @@
+<!--
+ * @file: TabBarItem
+ * @Dsc: 标题栏
+ * @Author wjz
+ * @Date 2021/9/1 23:21
+-->
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <slot v-if="!isActive" name="item-icon"></slot>
+    <slot v-else name="item-active"></slot>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'TabBarItem',
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red',
+    },
+  },
+  data () {
+    return {
+      //isActive:true
+    }
+  }, methods: {
+    itemClick () {
+      this.$router.replace(this.path).catch(err => err)
+    },
+  },
+  computed: {
+    isActive () {
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle () {
+      return this.isActive ? { color: this.activeColor } : {}
+    },
+  },
+}
+</script>
+
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+
+.active {
+  color: red;
+}
+</style>
