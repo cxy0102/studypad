3298212be13872141daee37626d531833ccbaba4
wjz <279646436@qq.com>
2021-09-02 14:42:29 +0800

    feat(首页模块): 首页界面

---
+++ src/views/main/index.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Project: cmis-pad-vue
+ * @File:index
+ * @Dsc: 描述
+ * @Author wjz
+ * @CreateDate 2021/9/2 9:18
+-->
+<template>
+  <div>
+    <main-nabar :ctitle="titleStr"></main-nabar>
+    <router-view></router-view>
+    <main-tabbar @tbclick="mnclick"></main-tabbar>
+  </div>
+
+</template>
+
+<script>
+
+import MainNabar from "@/components/content/mainNarbar/MainNabar";
+import MainTabbar from "@/components/content/mainTabbar/MainTabbar";
+
+export default {
+  name: "index",
+
+  components:{
+    MainNabar,MainTabbar
+  },
+  data() {
+    return {
+      titleStr:'干部信息'
+    };
+  },methods:{
+    mnclick(item){
+      console.log(item);
+      this.titleStr=item;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
