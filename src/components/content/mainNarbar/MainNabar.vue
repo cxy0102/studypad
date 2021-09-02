5be516b381aca795b794f645c4ad5c8eed9bf676
wjz <279646436@qq.com>
2021-09-02 14:41:55 +0800

    feat(首页模块): 实现导航栏

---
+++ src/components/content/mainNarbar/MainNabar.vue
@@ -0,0 +1,40 @@
+<!--
+ * @Project: cmis-pad-vue
+ * @File:MainNabar
+ * @Dsc: 描述
+ * @Author wjz
+ * @CreateDate 2021/9/2 11:07
+-->
+<template>
+  <div>
+    <van-nav-bar
+      :title=ctitle
+      left-text="返回"
+      right-text="按钮"
+      left-arrow
+      @click-left="onClickLeft"
+      @click-right="onClickRight"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainNabar",
+  props:[
+    'ctitle'
+  ],
+  methods: {
+    onClickLeft() {
+      Toast('返回');
+    },
+    onClickRight() {
+      Toast('按钮');
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
