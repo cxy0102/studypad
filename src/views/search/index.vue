a74f52a5d13c83d8f1f0b0de062f14985829037e
wjz <279646436@qq.com>
2021-09-02 14:43:01 +0800

    feat(首页模块): 干部搜索界面

---
+++ src/views/search/index.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Project: cmis-pad-vue
+ * @File:index
+ * @Dsc: 描述
+ * @Author wjz
+ * @CreateDate 2021/9/2 9:14
+-->
+<template>
+  <h1><span>干部搜索</span></h1>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
