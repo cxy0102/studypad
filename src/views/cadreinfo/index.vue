30c7140808c753316bedec1762c24fd8cfc6cbe4
wjz <279646436@qq.com>
2021-09-02 14:42:50 +0800

    feat(首页模块): 干部信息界面

---
+++ src/views/cadreinfo/index.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Project: cmis-pad-vue
+ * @File:index
+ * @Dsc: 描述
+ * @Author wjz
+ * @CreateDate 2021/9/2 9:14
+-->
+<template>
+  <span>干部信息</span>
+</template>
+
+<script>
+export default {
+  name: "index",
+  props:[
+    {
+
+    }
+  ]
+
+}
+</script>
+
+<style scoped>
+
+</style>
